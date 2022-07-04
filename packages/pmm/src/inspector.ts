54906fee99ef7bb81484803641f306a27079a011
Eamon Hyland <ehyland90@gmail.com>
2022-07-05 08:01:29 +1000

    Add e2e test for pin

--- packages/pmm/src/inspector.ts
+++ packages/pmm/src/inspector.ts
@@ -7,7 +7,7 @@ export async function findPackageManagerSpec() {
   let current = process.cwd();
 
   while (current !== path.dirname(current)) {
-    const packageInfo = await getPackageInDir(current);
+    const packageInfo = await loadPackageInDir(current);
     const specString = packageInfo?.packageManager;
 
     if (typeof specString === 'string') {
@@ -19,7 +19,7 @@ export async function findPackageManagerSpec() {
   }
 }
 
-async function getPackageInDir(dir: string) {
+async function loadPackageInDir(dir: string) {
   try {
     const packageString = await fs.readFile(
       path.resolve(dir, 'package.json'),
@@ -29,6 +29,18 @@ async function getPackageInDir(dir: string) {
   } catch (error: any) {
     if (error.code === 'ENOENT') {
       return undefined;
+    }
+    throw error;
+  }
+}
+
+export async function checkPackageExists(dir: string) {
+  try {
+    const stat = await fs.stat(path.resolve(dir, 'package.json'));
+    return stat.isFile();
+  } catch (error: any) {
+    if (error.code === 'ENOENT') {
+      return false;
     }
     throw error;
   }
